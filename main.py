39c3fa012bf7fcf35735199e103c50185ae33da7
AmaniAli <79951845+AmaniAli95@users.noreply.github.com>
2023-10-01 14:50:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,21 @@ from selenium import webdriver
 from utils.chrome-setup import setup_chrome_options
 from web-scraping.billplz import login_to_billplz,download_billplz_data,read_billplz_data
 from data-analysis.analyze-data import analyzedata
+from dotenv import load_dotenv
 
-ROOT_DIR = '/your/root/path/documents'
-link = 'your_link_here'
-username = 'your_username_here'
-password = 'your_password_here'
+load_dotenv()
+
+ROOT_DIR = os.environ.get("ROOT_DIR")
+LINK = os.environ.get("LINK")
+USERNAME = os.environ.get("USERNAME")
+PASSWORD = os.environ.get("PASSWORD")
+PATH = os.environ.get("PATH")
 
 def main():
     options = setup_chrome_options()
     download_dir = os.path.join(ROOT_DIR, 'Deploy Billplz')
-    path = '/path/to/your/chromedriver'
-    driver = webdriver.Chrome(executable_path=path, options=options)
-    login_to_billplz(driver, link, username, password)
+    driver = webdriver.Chrome(executable_path=PATH, options=options)
+    login_to_billplz(driver, LINK, USERNAME, PASSWORD)
     download_billplz_data(driver, download_dir, 5)
     df_billplz_myfa = read_billplz_data(download_dir, 'm')
     df_billplz_myfa['TYPE_OF_ACCOUNT'] = 'm'
